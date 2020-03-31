e26c51a19c9d7e47f367645c01db6b4fc91e3fe7
FThelin <fredrikthelin82@gmail.com>
2020-03-31 08:26:12 +0200

    Updates

---
+++ precache-manifest.373772e361218544f57612b48d4e2271.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9188eb412423a05aecc5aeca03340beb",
+    "url": "/leverans/index.html"
+  },
+  {
+    "revision": "d05e54facd0cbb17e33f",
+    "url": "/leverans/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "e6eb68d3d2e325a030a5",
+    "url": "/leverans/static/js/2.c3ecf80a.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/leverans/static/js/2.c3ecf80a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d05e54facd0cbb17e33f",
+    "url": "/leverans/static/js/main.5b7f1702.chunk.js"
+  },
+  {
+    "revision": "df5638d71bca25c19dbb",
+    "url": "/leverans/static/js/runtime-main.749f1af8.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/leverans/static/media/logo.5d5d9eef.svg"
+  }
+]);
